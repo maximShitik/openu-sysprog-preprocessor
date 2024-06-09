36c6b0de94ba3d009efb84a78ffa6712d4df9125
maximShitik <168300615+maximShitik@users.noreply.github.com>
2024-06-09 18:14:48 +0300

    Create lexer.h
    נבנה עץ

---
+++ lexer.h
@@ -0,0 +1,100 @@
+
+
+typedef struct ast
+{
+
+    enum line_type
+    {
+        
+        error_line = 1,
+        note_line ,
+        inst_line ,
+        command_line ,
+        empty_line 
+
+    } line_type;
+
+    union
+    {
+
+        struct inst
+        {
+            enum
+            {
+                data = 1,
+                string,
+                entry,
+                extrn,
+                lable
+            } inst_type;
+            union
+            {
+                char *label[32]; /*entry/extern*/
+                int data;
+                char *string[50];
+            } inst_opt;
+        } inst;
+
+        struct command
+        {
+            enum
+            {
+                mov,
+                cmp,
+                add,
+                sub,
+                nt,
+                clr,
+                lea,
+                inc,
+                dec,
+                jmp,
+                bne,
+                red,
+                prn,
+                jsr,
+                rts,
+                stop
+            } opcode;
+
+            struct
+            {
+                enum
+                {
+                    none,
+                    number,
+                    reg,
+                    label
+
+                } command_type;
+                union
+                {
+                    char *label[50];
+                    int number;
+                    int reg;
+                } command_operand;
+
+            } opcode_type[2];
+
+        } command;
+
+        int note;
+    }line_type_data;
+    struct
+    {
+        char error[300];
+        int line_number;
+    } error;
+    struct
+    {
+        enum
+        {
+            A,
+            R,
+            E
+        } ARE_type;
+    } ARE;
+
+} ast;
+
+struct ast parse_line(char *line);
