17d47b7bb3bfd9cd2a60eda4c714bb7b210c8687
maximShitik <168300615+maximShitik@users.noreply.github.com>
2024-07-14 21:32:56 +0300

    Update data_structs.h

--- data_structs.h
+++ data_structs.h
@@ -13,17 +13,15 @@ typedef struct hash
     char *macro_name;
     line_node *lines;
     struct hash *next;
+    
 } hash;
 
-typedef struct hash_table
-{
-    hash *hash_table[HASH_SIZE];
-} hash_table;
+
 
 unsigned int hash_function(char *macro_name);
 void hash_reset(hash *hash_table[]);
 void insert_to_hash(char *data, hash *hash_table[], int index, int is_macro_name);
 void free_memory(hash *hash_table[]);
-hash *search_macro_in_hash(char *macro_name, hash *hash_table[],int index);
+hash *search_macro_in_hash(char *macro_name, hash *hash_table[]);
 
 #endif
