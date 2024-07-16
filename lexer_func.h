b6147def88295ba0a07b2260f6c6172ca324b6bc
maximShitik <168300615+maximShitik@users.noreply.github.com>
2024-07-16 19:30:00 +0300

    Create lexer_func.h

---
+++ lexer_func.h
@@ -0,0 +1,20 @@
+#ifndef LEXER_FUNC_H
+#define LEXER_FUNC_H
+#include "lexer.h"
+#include "help_func.h"
+
+void error_found(struct ast *ast, char *error_message);
+struct ast set_label(struct ast *ast, struct sep_line sep, int index);
+struct ast set_lea(struct ast *ast, struct sep_line sep);
+int process_token(char *line, struct ast *ast);
+int is_int(char *line, struct ast *ast);
+int is_string(char *line, struct ast *ast);
+void reset_ast(struct ast *node);
+void error_found(struct ast *ast, char *error_message);
+struct ast set_entry_extern(struct ast *ast, struct sep_line sep);
+struct ast set_label(struct ast *ast, struct sep_line sep, int index);
+int check_command_or_instruction(struct ast *ast);
+struct ast set_data(struct ast *ast, struct sep_line sep);
+void set_instruction(struct ast *ast, struct sep_line sep);
+void set_command_name(struct ast *ast, char *command);
+#endif
