2017b0ad5607cfdf8482ef59945c2d5f918cadf5
maximShitik <168300615+maximShitik@users.noreply.github.com>
2024-07-16 19:29:29 +0300

    Create help_func.h

---
+++ help_func.h
@@ -0,0 +1,8 @@
+#ifndef HELP_FUNC_H
+#define HELP_FUNC_H
+void trim_whitespace(char *str);
+struct sep_line next_word(char *line);
+int contains_uppercase(const char *str);
+int system_names(char *line);
+void reset_string_array(char **array, int size);
+#endif
