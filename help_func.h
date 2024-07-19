b2fc3f13651c31000e6de028277936d7cca8a7d0
maximShitik <168300615+maximShitik@users.noreply.github.com>
2024-07-19 18:34:05 +0300

    Update help_func.h

--- help_func.h
+++ help_func.h
@@ -5,4 +5,5 @@ struct sep_line next_word(char *line);
 int contains_uppercase(const char *str);
 int system_names(char *line);
 void reset_string_array(char **array, int size);
+void remove_backslashes(char *str);
 #endif
