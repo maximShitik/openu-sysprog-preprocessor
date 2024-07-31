fe06f94794e8c666b51dab85e64a266262246337
maximShitik <168300615+maximShitik@users.noreply.github.com>
2024-07-31 20:44:04 +0300

    Update second_pass.c
    add the first word function

--- second_pass.c
+++ second_pass.c
@@ -1,5 +1,5 @@
 #include "first_pass.h"
-#include <string.h>
+#include "lexer.h"
 
 void convert_opcode_to_char(char *command, struct ast *ast)
 {
@@ -36,8 +36,51 @@ void convert_opcode_to_char(char *command, struct ast *ast)
     else if (ast->line_type_data.command.opcode == stop)
         command = "stop";
 }
+void set_first_word(struct ast line_ast, struct translation_unit *program)
+{
+    int i;
+    i = 0;
+    program->code_array[program->IC] = line_ast.line_type_data.command.opcode << 11;
+    
 
-int second_pas(struct translation_unit *program, const char *file_name, FILE *am_file)
+        if (line_ast.line_type_data.command.opcode_type[0].command_type == number)
+        {
+            program->code_array[program->IC] |= 1 << 7;
+        }
+        else if (line_ast.line_type_data.command.opcode_type[0].command_type == label)
+        {
+            program->code_array[program->IC] |= 1 << 8;
+        }
+        else if (line_ast.line_type_data.command.opcode_type[0].command_type == reg)
+        {
+            program->code_array[program->IC] |= 1 << 9;
+        }
+        else if (line_ast.line_type_data.command.opcode_type[0].command_type == reg_direct)
+        {
+            program->code_array[program->IC] |= 1 << 10;
+        }
+
+        if(line_ast.line_type_data.command.opcode_type[1].command_type == none)return;
+        if (line_ast.line_type_data.command.opcode_type[1].command_type == number)
+        {
+            program->code_array[program->IC] |= 1 << 3;
+        }
+        else if (line_ast.line_type_data.command.opcode_type[1].command_type == label)
+        {
+            program->code_array[program->IC] |= 1 << 4;
+        }
+        else if (line_ast.line_type_data.command.opcode_type[1].command_type == reg)
+        {
+            program->code_array[program->IC] |= 1 << 5;
+        }
+        else if (line_ast.line_type_data.command.opcode_type[1].command_type == reg_direct)
+        {
+            program->code_array[program->IC] |= 1 << 6;
+        }
+    
+    program->IC++;
+}
+int second_pass(char *file_name, FILE *am_file, struct translation_unit *program, struct hash *hash_table[])
 {
     char line[MAX_LINE];
     int is_error;
@@ -50,21 +93,13 @@ int second_pas(struct translation_unit *program, const char *file_name, FILE *am
 
     while (fgets(line, sizeof(line), am_file))
     {
-        if (line[0] == ';' || line[0] == '\n' || line[0] == '\r')
-        { /*skipping the eampty lines and note lines*/
-            continue;
-        }
-        line_ast = parse_line(line); /*parsing the line to an AST*/
+        line_ast = parse_line(line, hash_table); /*parsing the line to an AST*/
 
         /*building the first word*/
         if (line_ast.line_type == command)
         {
-            convert_opcode_to_char(char_command, &line_ast);
-
-            /*setting the opcode tramslated to oktal*/
-            program->code_array[program->IC] = 
-            program->code_array[program->IC] = *char_command<<5;
-
+            set_first_word(line_ast, program);
         }
     }
+    return 0;
 }
