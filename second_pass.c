c6cb0d44285fbd7d03a8549867700194fde4f0fa
maximShitik <168300615+maximShitik@users.noreply.github.com>
2024-08-01 21:06:30 +0300

    Update second_pass.c
    finished the seconad pass that will check for logical erros such as missing label using and will set the commands

--- second_pass.c
+++ second_pass.c
@@ -1,85 +1,68 @@
 #include "first_pass.h"
 #include "lexer.h"
+#include <stdlib.h>
+#include <string.h>
+#include "second_pass.h"
 
-void convert_opcode_to_char(char *command, struct ast *ast)
-{
-    if (ast->line_type_data.command.opcode == mov)
-        command = "mov";
-    else if (ast->line_type_data.command.opcode == cmp)
-        command = "cmp";
-    else if (ast->line_type_data.command.opcode == add)
-        command = "add";
-    else if (ast->line_type_data.command.opcode == sub)
-        command = "sub";
-    else if (ast->line_type_data.command.opcode == lea)
-        command = "lea";
-    else if (ast->line_type_data.command.opcode == clr)
-        command = "clr";
-    else if (ast->line_type_data.command.opcode == nt)
-        command = "not";
-    else if (ast->line_type_data.command.opcode == inc)
-        command = "inc";
-    else if (ast->line_type_data.command.opcode == dec)
-        command = "dec";
-    else if (ast->line_type_data.command.opcode == jmp)
-        command = "jmp";
-    else if (ast->line_type_data.command.opcode == bne)
-        command = "bne";
-    else if (ast->line_type_data.command.opcode == red)
-        command = "red";
-    else if (ast->line_type_data.command.opcode == prn)
-        command = "prn";
-    else if (ast->line_type_data.command.opcode == jsr)
-        command = "jsr";
-    else if (ast->line_type_data.command.opcode == rts)
-        command = "rts";
-    else if (ast->line_type_data.command.opcode == stop)
-        command = "stop";
-}
 void set_first_word(struct ast line_ast, struct translation_unit *program)
 {
-    int i;
-    i = 0;
-    program->code_array[program->IC] = line_ast.line_type_data.command.opcode << 11;
-    
+    OP_CODE_SET
 
-        if (line_ast.line_type_data.command.opcode_type[0].command_type == number)
-        {
-            program->code_array[program->IC] |= 1 << 7;
-        }
-        else if (line_ast.line_type_data.command.opcode_type[0].command_type == label)
-        {
-            program->code_array[program->IC] |= 1 << 8;
-        }
-        else if (line_ast.line_type_data.command.opcode_type[0].command_type == reg)
-        {
-            program->code_array[program->IC] |= 1 << 9;
-        }
-        else if (line_ast.line_type_data.command.opcode_type[0].command_type == reg_direct)
-        {
-            program->code_array[program->IC] |= 1 << 10;
-        }
+    if (line_ast.line_type_data.command.opcode_type[FIRST_WORD].command_type == number)
+    {
+        SOURCE_METHOD_0
+    }
+    else if (line_ast.line_type_data.command.opcode_type[FIRST_WORD].command_type == label)
+    {
+        SOURCE_METHOD_1
+    }
+    else if (line_ast.line_type_data.command.opcode_type[FIRST_WORD].command_type == reg)
+    {
+        SOURCE_METHOD_2
+    }
+    else if (line_ast.line_type_data.command.opcode_type[FIRST_WORD].command_type == reg_direct)
+    {
+        SOURCE_METHOD_3
+    }
 
-        if(line_ast.line_type_data.command.opcode_type[1].command_type == none)return;
-        if (line_ast.line_type_data.command.opcode_type[1].command_type == number)
-        {
-            program->code_array[program->IC] |= 1 << 3;
-        }
-        else if (line_ast.line_type_data.command.opcode_type[1].command_type == label)
-        {
-            program->code_array[program->IC] |= 1 << 4;
-        }
-        else if (line_ast.line_type_data.command.opcode_type[1].command_type == reg)
-        {
-            program->code_array[program->IC] |= 1 << 5;
-        }
-        else if (line_ast.line_type_data.command.opcode_type[1].command_type == reg_direct)
-        {
-            program->code_array[program->IC] |= 1 << 6;
-        }
-    
-    program->IC++;
+    if (line_ast.line_type_data.command.opcode_type[SECOND_WORD].command_type == none)
+        return;
+    if (line_ast.line_type_data.command.opcode_type[SECOND_WORD].command_type == number)
+    {
+        TARGET_METHOD_0
+    }
+    else if (line_ast.line_type_data.command.opcode_type[SECOND_WORD].command_type == label)
+    {
+        TARGET_METHOD_1
+    }
+    else if (line_ast.line_type_data.command.opcode_type[SECOND_WORD].command_type == reg)
+    {
+        TARGET_METHOD_2
+    }
+    else if (line_ast.line_type_data.command.opcode_type[SECOND_WORD].command_type == reg_direct)
+    {
+        TARGET_METHOD_3
+    }
+
+    ARE
+
+        program->IC++;
 }
+
+struct ext *ext_search(struct ext *head, char *name)
+{
+    struct ext *current = head;
+    while (current != NULL)
+    {
+        if (strcmp(current->ext_name, name) == 0)
+        {
+            return current;
+        }
+        current = current->next;
+    }
+    return NULL;
+}
+
 int second_pass(char *file_name, FILE *am_file, struct translation_unit *program, struct hash *hash_table[])
 {
     char line[MAX_LINE];
@@ -88,18 +71,97 @@ int second_pass(char *file_name, FILE *am_file, struct translation_unit *program
     struct ast line_ast = {0};
     struct symbol *symbol_found;
     struct ext *ext_found;
+    struct ext *new_ext;
     int i;
     char char_command[MAX_LINE];
+    line_number = 0;
+    is_error = 0;
 
     while (fgets(line, sizeof(line), am_file))
     {
         line_ast = parse_line(line, hash_table); /*parsing the line to an AST*/
 
-        /*building the first word*/
         if (line_ast.line_type == command)
         {
             set_first_word(line_ast, program);
+
+            if (SOURCE_AND_TARGET_ARE_REGISTERS)
+            {
+                i = 0;
+                SET_SOURCE.regg << BITS_6_8;
+                SET_TARGET.regg << BITS_3_5;
+                ARE
+            }
+            else
+            {
+                
+                for(i = 0; i < 2; i++)
+                {
+                    if (COMMAND_TYPE == number)
+                    {
+                        SET_ADRESS.numberr << 3;
+                        ARE
+
+                          
+                    }
+                    else if (COMMAND_TYPE == reg || COMMAND_TYPE == reg_direct)
+                    {
+                        if (i == 0)
+                        {
+                            SET_SOURCE.regg << BITS_6_8;
+                        }
+                        else
+                        {
+                            SET_TARGET.regg << BITS_3_5;
+                        }
+                       
+                       
+                    }
+                    else if (COMMAND_TYPE == label)
+                    {
+                        symbol_found = symbol_search(program->symbol_table, line_ast.line_type_data.command.opcode_type[i].labell[0]);
+                        if (symbol_found)
+                        {
+                            program->code_array[program->IC] = symbol_found->address << 3;
+                            if (symbol_found->symbol_type == extrn_type)
+                            {
+                                program->code_array[program->IC] |= 1; /*adding the E*/
+                                ext_found = ext_search(program->ext_table, line_ast.line_type_data.command.opcode_type[i].labell[0]);
+                                if (ext_found)
+                                {
+                                    ext_found->address = program->IC + 100;
+                                    new_ext->address_counter++;
+                                }
+                                else
+                                {
+                                    new_ext = (struct ext *)malloc(sizeof(struct ext));
+                                    strcpy(new_ext->ext_name, line_ast.line_type_data.command.opcode_type[i].labell[0]);
+                                    new_ext->address = program->IC + 100;
+                                    new_ext->exter_count++;
+                                    new_ext->next = program->ext_table;
+                                    new_ext->address_counter++;
+                                    program->ext_table = new_ext;
+                                }
+                            }
+                            else
+                            {
+                                program->code_array[program->IC] |= 2;
+                            }
+                        }
+                        else
+                        {
+                            printf("Error in %s line %d: %s is not defined label \n", file_name, line_number, line_ast.line_type_data.command.opcode_type[i].labell[0]);
+                              is_error = 1;
+                        }
+                    }
+                    if(COMMAND_TYPE!=none)
+                    {
+                         program->IC++;
+                    }
+                }
+            }
         }
+        line_number++;
     }
     return 0;
 }
