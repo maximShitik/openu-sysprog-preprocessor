d779310f9a52b41941098cde01df825f872b1df6
maximShitik <168300615+maximShitik@users.noreply.github.com>
2024-08-04 13:34:04 +0300

    Create back_end.h

---
+++ back_end.h
@@ -0,0 +1,11 @@
+
+#include "first_pass.h"
+#include "second_pass.h"
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+int decimal_to_octal(int decimalNumber);
+void print_object_file(char *file_name, struct translation_unit *);
+
+
