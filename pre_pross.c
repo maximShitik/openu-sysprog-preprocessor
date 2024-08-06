592dd156b422dee69ba0f5f56bd730dfb87a7526
maximShitik <168300615+maximShitik@users.noreply.github.com>
2024-08-07 00:50:08 +0300

    Update pre_pross.c

--- pre_pross.c
+++ pre_pross.c
@@ -7,6 +7,7 @@
 #include "data_structs.c"
 #include "pre_pross.h"
 #include "help_func.h"
+#include "macros.h"
 
 int line_defenition(char *line, struct sep_line separated, char error[MAX_LINE])
 {
@@ -54,37 +55,63 @@ int line_defenition(char *line, struct sep_line separated, char error[MAX_LINE])
     }
 }
 
-void find_macro(char *line, hash *hash_table[], FILE *input, FILE *output, char error[MAX_LINE])
+char *pre_prossesor(char *line, hash *hash_table[], char *input)
 {
     int index_copy;
+    int line_number;
     hash *macro_found;
     int indicator; /*if indicator is macro_defenition its will define the lines as macros till the indicator is regular*/
     int name_index;
-    name_index = 0;
+    struct sep_line separated;
     char *line_copy;
+    FILE *am_file;
+    FILE *as_file;
+    char *as_file_name;
+    char *am_file_name;
+    char error[MAX_LINE];
+    line_node *current_line;
+    int error_flag;
+    error_flag = 0;
+    name_index = 0;
+    indicator = first_int;
     index_copy = 0;
+    line_number = 0;
 
-    while (fgets(line, MAX_LINE, input))
+    as_file_name = STRCAT_MALLOC(as_file_name, input, ".as");
+    am_file_name = STRCAT_MALLOC(am_file_name, input, ".am");
+    as_file = fopen(input, "r");
+    am_file = fopen(am_file_name, "w");
+    if (as_file_name == NULL || am_file_name == NULL || am_file == NULL || as_file == NULL)
     {
+        printf("Error opening files\n");
+        remove(am_file_name);
+        free(as_file_name);
+        free(am_file_name);
 
+        return NULL;
+    }
+
+    while (fgets(line, MAX_LINE, as_file) != NULL)
+    {
+        line_number++;
         trim_whitespace(line);
         line_copy = malloc(strlen(line) + 1);
         if (line_copy == NULL)
         {
-            strcpy(error, "Memory allocation error");
-            exit(EXIT_FAILURE);
+            printf("Memory allocation error");
+            error_flag = 1;
         }
         if (line[0] == '\0' || line[0] == ';')
             continue;
         strcpy(line_copy, line);
 
-        struct sep_line separated = next_word(line);
+        separated = next_word(line);
         name_index = line_defenition(line_copy, separated, error);
 
         if (name_index == Error)
         {
-            free(line_copy);
-            return;
+            printf("Error in pre-prossesor: %s in line %d \n", error, line_number);
+            error_flag = 1;
         }
         else if (name_index >= min_hash_index)
         {
@@ -112,23 +139,35 @@ void find_macro(char *line, hash *hash_table[], FILE *input, FILE *output, char 
                 if (macro_found != NULL)
                 {
                     /*add the hash data to the as file*/
-                    line_node *current_line = macro_found->data;
+                    current_line = macro_found->data;
                     while (current_line != NULL)
                     {
-                        fprintf(output, "%s\n", current_line->line_data);
+                        fprintf(am_file, "%s\n", current_line->line_data);
                         current_line = current_line->next;
                     }
-                    fflush(output);
+                    fflush(am_file);
                 }
                 /*add the line to the as file*/
                 else
                 {
-                    fprintf(output, "%s\n", line_copy);
-                    fflush(output);
+                    fprintf(am_file, "%s\n", line_copy);
+                    fflush(am_file);
                 }
             }
         }
     }
-    free(line_copy);
+    if (error_flag)
+    {
+        FREE_AND_REMOVE
+        return NULL;
+    }
+    else
+    {
+        free(line_copy);
+        free(as_file_name);
+        fclose(as_file);
+        fclose(am_file);
+        return am_file_name;
+    }
 }
 #endif
