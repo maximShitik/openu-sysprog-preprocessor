78a4f4e8cd1bf32f79ae3d60ce694fdb61624d84
maximShitik <168300615+maximShitik@users.noreply.github.com>
2024-08-07 00:50:52 +0300

    Update second_pass.h

--- second_pass.h
+++ second_pass.h
@@ -1,3 +1,6 @@
+#ifndef SECOND_PASS_H
+#define SECOND_PASS_H
 #include "first_pass.h"
 int second_pass(char *file_name, FILE *am_file, struct translation_unit *program, struct hash *hash_table[]);
 void free_translation_unit(struct translation_unit *program);
+#endif
