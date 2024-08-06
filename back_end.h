cb20c92dd603368aec3df01697c7c3ded2737263
maximShitik <168300615+maximShitik@users.noreply.github.com>
2024-08-06 16:35:03 +0300

    Update back_end.h

--- back_end.h
+++ back_end.h
@@ -5,7 +5,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-int decimal_to_octal(int decimalNumber);
 void print_object_file(char *file_name, struct translation_unit *);
-
+void print_ext_file(char *file_name, struct translation_unit *program);
+void print_entry_file(char *file_name, struct translation_unit *program);
+void printing_files(char *file_name, struct translation_unit *program);
 
