01cfe23ccc799ebab1fe76497c302cddb12b3d55
maximShitik <168300615+maximShitik@users.noreply.github.com>
2024-08-07 23:26:55 +0300

    Update assembler.c

--- assembler.c
+++ assembler.c
@@ -1,73 +1,76 @@
 #ifndef ASSEMBLER_C
 #define ASSEMBLER_C
 
-#include "lexer.c"        /*before copyng to the ubuntu need to be changed to .h*/
-#include "pre_pross.c"    /*before copyng to the ubuntu need to be changed to .h*/
-#include "data_structs.c" /*before copyng to the ubuntu need to be changed to .h*/
+#include "lexer.c"     /*before copyng to the ubuntu need to be changed to .h*/
+#include "pre_pross.c" /*before copyng to the ubuntu need to be changed to .h*/
 #include "first_pass.h"
 #include "first_pass.c"
 #include "second_pass.c"
 #include "back_end.h"
 #include "back_end.c"
-
+#include "macros.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include "data_structs.h"
 
 int main(int argc, char **argv)
 {
     int i;
-    struct translation_unit program = {0};
+    struct translation_unit *program;
     char line[MAX_LINE];
     char *am_file_name;
     hash *hash_table[HASH_SIZE] = {0};
     FILE *am_file;
-    FILE *as_file;
+    int original_line_numbers[MAX_ADDRESS] = {0};
+    int expanded_line_count = 0;
+
     for (i = 1; i < argc; i++)
     {
-        printf("Input file: %s\n", argv[i]);
-       
+        program = create_translation_unit();
+        first_intilization(program);
         hash_reset(hash_table);
-        am_file_name = pre_prossesor(line, hash_table,  argv[i]);
+        am_file_name = pre_prossesor(line, hash_table, argv[i], original_line_numbers, &expanded_line_count);
         if (am_file_name != NULL)
         {
             am_file = fopen(am_file_name, "r");
             if (am_file == NULL)
             {
+                free_translation_unit(program);
+                free(am_file_name);
+                free_hash(hash_table);
                 fprintf(stderr, "Error: failed to open file %s\n", am_file_name);
-                free_hash(hash_table);
-                return EXIT_FAILURE;
+                continue;
             }
-            else if (first_pass(am_file_name, am_file, &program, hash_table))
+
+            else if (first_pass(am_file_name, am_file, program, hash_table, original_line_numbers, expanded_line_count))
             {
-                fclose(am_file);
-                free_translation_unit(&program);
-                free_hash(hash_table);
+                FREE_TRANSU_HASH_CLOSE
                 remove(am_file_name);
-                return EXIT_FAILURE;
+                continue;
             }
             else
 
             {
                 rewind(am_file);
-                if (second_pass(am_file_name, am_file, &program, hash_table))
-                {
-                    free_translation_unit(&program);
-                    free_hash(hash_table);
-                    fclose(am_file);
-                    remove(am_file_name);
+                if (second_pass(am_file_name, am_file, program, hash_table))
+                {   remove(am_file_name);
+                    FREE_TRANSU_HASH_CLOSE
+                    
+                    continue;
                 }
                 else
-                    printing_files(am_file_name, &program);
+                    printing_files(am_file_name, program);
             }
-            fclose(am_file);
-            free_translation_unit(&program);
+            FREE_TRANSU_HASH_CLOSE
+        }
+        else
+        {
             free_hash(hash_table);
-            free(am_file_name);
+            free_translation_unit(program);
+            remove(am_file_name);
         }
     }
     return 0;
 }
-
 #endif
