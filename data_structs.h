256a53c72c0c3ad24e2e4664c096acf698e10cfe
maximShitik <168300615+maximShitik@users.noreply.github.com>
2024-08-07 23:27:31 +0300

    Update data_structs.h

--- data_structs.h
+++ data_structs.h
@@ -2,6 +2,7 @@
 #define DATA_STRUCTS_H
 #define HASH_SIZE 100
 #define MAX_LINE 81
+#define MAX_ADDRESS 1000
 
 typedef struct symbol symbol;
 typedef struct symbol_table symbol_table;
@@ -35,6 +36,14 @@ struct symbol
     } symbol_type;
     struct symbol *next;
 };
+
+typedef struct {
+    int original_line_number;
+    int is_macro_expansion;
+    char macro_name[MAX_LINE];
+    int macro_def_line_number;
+} line_info;
+
 struct address
 {
     int address;
@@ -45,7 +54,7 @@ struct address
 
 struct ext
 {
-    char ext_name[MAX_LINE];
+    char ext_name[MAX_ADDRESS];
     struct address *address_head;
     int address_counter;
     struct ext *next;
@@ -54,7 +63,7 @@ struct ext
 
 struct entry
 {
-    char entry_name[MAX_LINE];
+    char entry_name[MAX_ADDRESS];
     int address;
     struct entry *next;
     struct entry *prev;
@@ -62,8 +71,8 @@ struct entry
 
 struct translation_unit
 {
-    int code_array[MAX_LINE];
-    int data_array[MAX_LINE];
+    int code_array[MAX_ADDRESS];
+    int data_array[MAX_ADDRESS];
     struct symbol *symbol_table;
     struct ext *ext_table;
     int IC;
@@ -72,10 +81,26 @@ struct translation_unit
     int entry_count;
     int exter_count;
 };
-
+/*HASH*/
 unsigned int hash_function(char *name);
 void hash_reset(hash *hash_table[]);
 void insert_to_hash(char *data, hash *hash_table[], int index, int is_name);
 void free_hash(hash *hash_table[]);
 hash *search_in_hash(char *macro_name, hash *hash_table[]);
+
+
+
+/*---------LINED LIST FUNC------------*/
+void symbol_insert(struct symbol **head, struct symbol *new_symbol);
+struct symbol *symbol_search(struct symbol *head, char *name);
+void free_translation_unit(struct translation_unit *program);
+void add_entry(struct translation_unit *program, char *symbol_name, int address);
+struct translation_unit *create_translation_unit();
+struct entry *create_entry();
+struct ext *create_ext();
+struct symbol *create_symbol();
+void add_extr(struct translation_unit *program, char *symbol_name, int address);
+struct ext *ext_search(struct ext *head, char *name);
+void add_address(struct translation_unit *program, struct ext *ext_found, int address);
+void first_intilization(struct translation_unit *program);
 #endif
