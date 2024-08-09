6aec26d7a16520ee8113e5d9a8d0af6d8b7d8eb7
maximShitik <168300615+maximShitik@users.noreply.github.com>
2024-08-09 20:56:08 +0300

    Update back_end.h

--- back_end.h
+++ back_end.h
@@ -1,12 +1,12 @@
-
-#include "first_pass.h"
-#include "second_pass.h"
-#include <math.h>
+#ifndef BACK_END_H
+#define BACK_END_H
+#include "first_and_second_pass.h"
 #include <stdlib.h>
 #include <string.h>
 
-void print_object_file(char *file_name, struct translation_unit *);
+void print_object_file(char *file_name, struct translation_unit *program);
 void print_ext_file(char *file_name, struct translation_unit *program);
 void print_entry_file(char *file_name, struct translation_unit *program);
 void printing_files(char *file_name, struct translation_unit *program);
 
+#endif
